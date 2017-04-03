529e83c10da67159a436efc874f7931b380bc762
shyamahira <shyamahira@bbtower.co.jp>
2017-04-04 06:46:55 +0900

    不要な行の削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,6 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render("#{association.to_s.singularize}_fields", f: builder)
     end
-    ### デバッグ
-    Rails.logger.debug("idデバッグ: #{id}")
-    Rails.logger.debug("fieldsデバッグ: #{fields}")
-    Rails.logger.debug("fields.gsubデバッグ: #{fields.gsub("\n", "")}")
-    ###
     link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
 end
