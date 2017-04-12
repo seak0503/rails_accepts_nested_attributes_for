bf647e239f5f7927b57f6d1ed3ea7d651fbf7f0a
Shota Yamahira <seak0503@gmail.com>
2017-04-13 06:25:18 +0900

    taskのセレクトフォームはuser独自のものなので、アトリビュートをtaskからuserに付け替え

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessor :select_title
+
   has_many :addresses
   accepts_nested_attributes_for :addresses, allow_destroy: true, reject_if: :all_blank
   has_many :tasks
