9a595da2a5441ab9618e19e6da88b20bfc329412
shyamahira <shyamahira@bbtower.co.jp>
2017-04-14 10:25:45 +0900

    リファクタリング

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,3 @@
 class Task < ActiveRecord::Base
   belongs_to :user
-
-  TITLES = %w(勉強 食事 仕事 睡眠 休憩)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :select_title
+  TITLES = %w(勉強 食事 仕事 睡眠 休憩)
 
   has_many :addresses
   accepts_nested_attributes_for :addresses, allow_destroy: true, reject_if: :all_blank
