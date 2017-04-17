5886e95e93338968cd195fb390014a263d347f78
Shota Yamahira <seak0503@gmail.com>
2017-04-18 06:31:06 +0900

    jquery完成

--- app/assets/javascripts/jquery.myplugin.js
+++ app/assets/javascripts/jquery.myplugin.js
@@ -1,27 +1,36 @@
 $.fn.extend({
   /**
-    * マルチセレクトフォームでselectedになっているvalをhiddenフォームに埋め込む
+    * railsAcceptsNestedAttributesForを使ったセレクトフォームでselectedになっているvalueを
+    * hiddenフォームに埋め込む
   */
   railsAcceptsNestedAttributesForSelectToHidden:function(options) {
     var settings = $.extend({
       'object'     : 'object_name',
       'association' : 'association_name',
-      'index'       : 0,
       'parameter'   : 'parameter'
     }, options);
+    var $this = $(this);
 
-    var $this = $(this),
-      vals = $this.val();
-      className = "rails-accepts-nested-attributes-for-select-to-hidden"
-    $this.siblings("input[type=hidden]." + className).remove();
-    if (vals) {
-      $.each(vals, function (i, val) {
-        var id = settings.object + "_" + settings.association + "_attributes_" + settings.index + "_" + settings.parameter,
-          name = settings.object + "[" + settings.association + "_attributes][" + settings.index + "][" + settings.parameter + "]";
-        $this.after($('<input type="hidden">').attr('id', id).attr('name', name).val(val).addClass(className));
-        settings.index += 1;
-      });
+    $(window).load(function () {
+      toHidden($this);
+    });
+
+    $this.change(function () {
+      toHidden($this);
+    });
+
+    function toHidden($this) {
+      var className = "rails-accepts-nested-attributes-for-select-to-hidden",
+        vals = $this.val();
+      $this.siblings("input[type=hidden]." + className).remove();
+      if (vals) {
+        $.each(vals, function (i, val) {
+          var id = settings.object + "_" + settings.association + "_attributes_" + i + "_" + settings.parameter,
+            name = settings.object + "[" + settings.association + "_attributes][" + i + "][" + settings.parameter + "]";
+          $this.after($('<input type="hidden">').attr('id', id).attr('name', name).val(val).addClass(className));
+        });
+      }
+      event.preventDefault();
     }
-    event.preventDefault();
   }
 });
