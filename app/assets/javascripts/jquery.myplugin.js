bfcf98f9de72ea81de60e0f7008eb8cd4832dae8
Shota Yamahira <seak0503@gmail.com>
2017-04-18 06:03:09 +0900

    ひとまずjqueryの拡張性を意識するのはやめた

--- app/assets/javascripts/jquery.myplugin.js
+++ app/assets/javascripts/jquery.myplugin.js
@@ -2,32 +2,26 @@ $.fn.extend({
   /**
     * マルチセレクトフォームでselectedになっているvalをhiddenフォームに埋め込む
   */
-  railsAcceptsNestedAttributesForForm:function(options) {
+  railsAcceptsNestedAttributesForSelectToHidden:function(options) {
     var settings = $.extend({
       'object'     : 'object_name',
       'association' : 'association_name',
       'index'       : 0,
-      'parameter'   : 'parameter',
-      'targetType' : 'hidden'
+      'parameter'   : 'parameter'
     }, options);
 
     var $this = $(this),
       vals = $this.val();
-    if (settings.targetType == 'hidden') {
-      toHidden($this, vals);
+      className = "rails-accepts-nested-attributes-for-select-to-hidden"
+    $this.siblings("input[type=hidden]." + className).remove();
+    if (vals) {
+      $.each(vals, function (i, val) {
+        var id = settings.object + "_" + settings.association + "_attributes_" + settings.index + "_" + settings.parameter,
+          name = settings.object + "[" + settings.association + "_attributes][" + settings.index + "][" + settings.parameter + "]";
+        $this.after($('<input type="hidden">').attr('id', id).attr('name', name).val(val).addClass(className));
+        settings.index += 1;
+      });
     }
     event.preventDefault();
-
-    function toHidden($this, vals) {
-      $this.siblings("input[type=hidden].rails-accepts-nested-attributes-for-form-hidden").remove();
-      if (vals) {
-        $.each(vals, function (i, val) {
-          var id = settings.object + "_" + settings.association + "_attributes_" + settings.index + "_" + settings.parameter,
-            name = settings.object + "[" + settings.association + "_attributes][" + settings.index + "][" + settings.parameter + "]";
-          $this.after($('<input type="hidden">').attr('id', id).attr('name', name).val(val).addClass("rails-accepts-nested-attributes-for-form-hidden"));
-          settings.index += 1;
-        });
-      }
-    }
   }
 });
